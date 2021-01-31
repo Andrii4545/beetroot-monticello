37a0ff09dc4e78366dc09795d88846f85553dcc0
Andrii4545 <goldwin454545@gmail.com>
2021-01-31 20:37:28 +0200

    slick responsive

--- js/main.js
+++ js/main.js
@@ -3,4 +3,21 @@ $('.multiple-items').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     dots: true, 
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          autoplay: false,
+        }
+      },
+      
+    ]
   });
